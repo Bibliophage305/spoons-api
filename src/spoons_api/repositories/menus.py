8e60d4e8bb2fac4fbeb9e9e30cefe356947ba775
Chris <christopher.hampson58@gmail.com>
2026-06-30 12:57:43 +0100

    Pass max_age to api

--- src/spoons_api/repositories/menus.py
+++ src/spoons_api/repositories/menus.py
@@ -1,19 +1,34 @@
+from datetime import datetime
+
 import spoons_api.api as api
 from spoons_api.models.menu import Menu, MenuSummary
 
 
-def all(franchise: str, venue_ref: int, sales_area_id: int) -> list[MenuSummary]:
+def all(
+    franchise: str,
+    venue_ref: int,
+    sales_area_id: int,
+    max_age: datetime.timedelta | None = None,
+) -> list[MenuSummary]:
     return [
         MenuSummary.from_dict(m)
         for m in api.request(
-            f"{franchise}/venues/{venue_ref}/sales-areas/{sales_area_id}/menus"
+            f"{franchise}/venues/{venue_ref}/sales-areas/{sales_area_id}/menus",
+            max_age=max_age,
         )["data"]
     ]
 
 
-def get(franchise: str, venue_ref: int, sales_area_id: int, menu_id: int) -> Menu:
+def get(
+    franchise: str,
+    venue_ref: int,
+    sales_area_id: int,
+    menu_id: int,
+    max_age: datetime.timedelta | None = None,
+) -> Menu:
     return Menu.from_dict(
         api.request(
-            f"{franchise}/venues/{venue_ref}/sales-areas/{sales_area_id}/menus/{menu_id}"
+            f"{franchise}/venues/{venue_ref}/sales-areas/{sales_area_id}/menus/{menu_id}",
+            max_age=max_age,
         )["data"]
     )
--- src/spoons_api/repositories/venues.py
+++ src/spoons_api/repositories/venues.py
@@ -1,10 +1,15 @@
+import datetime
+
 import spoons_api.api as api
 from spoons_api.models.venue import Venue, VenueSummary
 
 
-def all() -> list[VenueSummary]:
-    return [VenueSummary.from_dict(v) for v in api.request("venues")["data"]]
+def all(max_age: datetime.timedelta | None = None) -> list[VenueSummary]:
+    return [
+        VenueSummary.from_dict(v)
+        for v in api.request("venues", max_age=max_age)["data"]
+    ]
 
 
-def get(venue_ref: int) -> Venue:
-    return Venue.from_dict(api.request(f"venues/{venue_ref}")["data"])
+def get(venue_ref: int, max_age: datetime.timedelta | None = None) -> Venue:
+    return Venue.from_dict(api.request(f"venues/{venue_ref}", max_age=max_age)["data"])
